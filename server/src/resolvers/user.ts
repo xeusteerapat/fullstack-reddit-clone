ce0fdaa5d5e73f79a63d25545d7c11aa36b55183
xeusteerapat <xeus085@gmail.com>
2020-08-29 00:12:35 +0700

    [change] register mutation using knex query builder

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -11,6 +11,7 @@ import {
 import argon2 from 'argon2';
 import { User } from './../entities/User';
 import { MyContext } from './../types';
+import { EntityManager } from '@mikro-orm/postgresql';
 
 // Create an Input type
 @InputType()
@@ -83,14 +84,21 @@ export class UserResolver {
     }
 
     const hashedPassword = await argon2.hash(options.password);
-
-    const user = em.create(User, {
-      username: options.username,
-      password: hashedPassword,
-    });
+    let user;
 
     try {
-      await em.persistAndFlush(user);
+      const result = await (em as EntityManager)
+        .createQueryBuilder(User)
+        .getKnexQuery()
+        .insert({
+          username: options.username,
+          password: hashedPassword,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+        .returning('*');
+
+      user = result[0];
     } catch (err) {
       if (err.code === '23505') {
         return {
