c2147678b7503b102b205823618ba0db8f583e1e
xeusteerapat <xeus085@gmail.com>
2020-08-28 23:53:07 +0700

    [add] utils for create an errors validation

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -9,7 +9,7 @@ const client = createClient({
   },
 });
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ThemeProvider theme={theme}>
--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -11,6 +11,7 @@ import {
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface registerProps {}
 
@@ -24,8 +25,12 @@ const Register: React.FC<registerProps> = () => {
           username: '',
           password: '',
         }}
-        onSubmit={async values => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          }
         }}
       >
         {({ isSubmitting }) => (
