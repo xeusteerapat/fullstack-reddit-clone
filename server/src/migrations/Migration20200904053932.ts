0ad4bc167c8d08dca0c344bf977edc77190edcfd
xeusteerapat <xeus085@gmail.com>
2020-09-04 13:07:18 +0700

    [delete] wipe old data for adding new column

---
+++ server/src/migrations/Migration20200904053932.ts
@@ -0,0 +1,10 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20200904053932 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "user" add column "email" text not null;');
+    this.addSql('alter table "user" add constraint "user_email_unique" unique ("email");');
+  }
+
+}
