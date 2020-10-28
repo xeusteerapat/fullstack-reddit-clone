daf21b8ba31325d4e7b318ce0dea8927c79cdedd
xeusteerapat <xeus085@gmail.com>
2020-10-28 23:20:33 +0700

    [update] user create many posts

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -5,8 +5,10 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
   BaseEntity,
+  ManyToOne,
 } from 'typeorm';
 import { ObjectType, Field } from 'type-graphql';
+import { User } from './User';
 
 @ObjectType()
 @Entity()
@@ -15,6 +17,25 @@ export class Post extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @Field()
+  @Column({ type: 'int', default: 0 })
+  points!: number;
+
+  @Field()
+  @Column()
+  authorId: number;
+
+  @ManyToOne(() => User, user => user.posts)
+  author: User;
+
   @Field(() => String)
   @CreateDateColumn()
   createdAt: Date;
@@ -22,8 +43,4 @@ export class Post extends BaseEntity {
   @Field(() => String)
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column()
-  title!: string;
 }
