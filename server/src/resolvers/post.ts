681a9cebbc49d2b1e4df03455dec44a82645e3cb
xeusteerapat <xeus085@gmail.com>
2020-10-28 23:21:03 +0700

    [update] create post resolver with auth and authorId

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,5 +1,25 @@
-import { Resolver, Query, Arg, Int, Mutation } from 'type-graphql';
+import { isAuth } from './../middleware/isAuth';
+import { MyContext } from './../types';
+import {
+  Resolver,
+  Query,
+  Arg,
+  Int,
+  Mutation,
+  InputType,
+  Field,
+  Ctx,
+  UseMiddleware,
+} from 'type-graphql';
 import { Post } from './../entities/Post';
+
+@InputType()
+class PostInput {
+  @Field()
+  title: string;
+  @Field()
+  text: string;
+}
 
 @Resolver()
 export class PostResolver {
@@ -14,8 +34,15 @@ export class PostResolver {
   }
 
   @Mutation(() => Post)
-  async createPost(@Arg('title', () => String) title: string): Promise<Post> {
-    return Post.create({ title }).save();
+  @UseMiddleware(isAuth)
+  async createPost(
+    @Arg('input') input: PostInput,
+    @Ctx() { req }: MyContext
+  ): Promise<Post> {
+    return Post.create({
+      ...input,
+      authorId: req.session.userId,
+    }).save();
   }
 
   @Mutation(() => Post, { nullable: true })
