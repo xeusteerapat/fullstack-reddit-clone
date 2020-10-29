af9dd302f630c77480181efa37d3d589ff16aa43
xeusteerapat <xeus085@gmail.com>
2020-10-29 23:57:39 +0700

    [update] input field with textarea

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -4,25 +4,33 @@ import {
   FormLabel,
   Input,
   FormErrorMessage,
+  Textarea
 } from '@chakra-ui/core';
 import { useField } from 'formik';
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextArea = Input
+  if (textarea) {
+    InputOrTextArea = Textarea
+  }
+
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={!!error}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input {...field} {...props} id={field.name} />
+      <InputOrTextArea {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
