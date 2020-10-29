b67c58b8d37919836e1c6f5c38f77e0738cb3ace
xeusteerapat <xeus085@gmail.com>
2020-10-30 01:02:52 +0700

    [add] custom hook for verify authentication

---
+++ client/src/utils/useIsAuth.ts
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { useMeQuery } from '../generated/graphql';
+
+export const useIsAuth = () => {
+  const router = useRouter();
+
+  // check user authentication
+  const [{ data, fetching }] = useMeQuery();
+  useEffect(() => {
+    if (!fetching && !data?.me) {
+      router.replace('/login');
+    }
+  }, [data, router, fetching]);
+};
